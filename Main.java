58636d24487752a94ed57d2b7844d4fdd4158a2b
Lucas <lucasucb2020@gmail.com>
2021-12-16 09:09:50 -0300

    Chess project - Fist Commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        int x=12;
+
+        for(int i = 0; i < x; i++){
+            System.out.println(i*12);
+        }
+
+    }
+}
