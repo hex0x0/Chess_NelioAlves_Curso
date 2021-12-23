481df82a0c019bdd560f1e921b57512455375dfe
lucas <lucasucb2020@gmail.com>
2021-12-23 08:53:59 -0300

    Método para remover uma peça em uma posição

--- Board.java
+++ Board.java
@@ -50,6 +50,26 @@ public class Board {
 
     }
 
+    public Piece removePiece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Essa posição não existe!");
+        }
+
+        if(piece(position) == null){
+            return null;
+        }
+
+        Piece aux = piece(position);
+
+        aux.position = null;
+
+        pieces[position.getRow()][position.getColumn()] = null;
+
+        return aux;
+
+
+    }
+
     public boolean positionExists(int row, int column){
         return row >=0 && row < rows && column >=0 && column < columns;
     }
